a432528960610f7f5139bc51c1fe786328e00800
anggistaamaliayashinta <anggistaamalia@gmail.com>
2021-05-03 11:03:52 +0700

    pertemuan 12 || install laravel

---
+++ laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>BIODATA DIRI</title>
+</head>
+<body>
+<h1 align="center">BIODATA DIRI</h1>
+<table width="745" border="1" cellspacing="0" cellpadding="5" align="center">
+<tr align="center" bgcolor="#FFC0CB">
+<td width="174">DATA DIRI</td>
+<td width="353">KETERANGAN</td>
+</tr>
+<tr>
+<td>Nama</td>
+<td>Anggista Amalia Yashinta</td>
+</tr>
+<tr>
+<td>Jurusan</td>
+<td>Teknologi Informasi</td>
+</tr>
+<tr>
+<td>Program Studi</td>
+<td>Teknik Informatika</td>
+</tr>
+<tr>
+<td>Jenis Kelamin</td>
+<td>Perempuan</td>
+</tr>
+<tr>
+<td>Tempat/Tanggal Lahir</td>
+<td>Bangkalan, 14 Agustus 2000</td>
+</tr>
+<tr>
+<td>Perguruan Tinggi</td>
+<td>Politeknik Negeri Malang</td>
+</tr>
+<tr>
+<td>Alamat</td>
+<td>Perum Bangakalan Indah Blok MG.23 Bangakalan</td>
+</tr>
+<tr>
+<td>Status</td>
+<td>Mahasiswa</td>
+</tr>
+</table>
+</body>
+</html>
